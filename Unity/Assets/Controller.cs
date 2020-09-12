181b8cca4fdcf477e1717155fd61773dff5b377f
FedericoGarciaGarcia <65201698+FedericoGarciaGarcia@users.noreply.github.com>
2020-09-12 17:36:01 +0900

    Allow for multiple concurrent games

---
+++ Unity/Assets/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+ 
+public class Controller : MonoBehaviour
+{
+	public GameObject game;
+	public int subjectSize = 200;
+	
+	private long iteration = 0;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		//QualitySettings.vSyncCount = 0;  // VSync must be disabled
+		//Application.targetFrameRate = 60;
+		for(int i=0; i<subjectSize; i++)
+		{
+			Instantiate(game, new Vector3(0, 0, 0), Quaternion.identity);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			 SceneManager.LoadScene("Scene");
+		}
+		
+		iteration++;
+		
+		if(iteration % 50L == 0L)
+		{
+			Debug.Log(iteration);
+			Debug.Log(1.0f/Time.deltaTime);
+		}
+    }
+}
---
+++ Unity/Assets/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+ 
+public class GameController : MonoBehaviour
+{
+	public GameObject ball;
+	public bool gameOver = false;
+	public float points;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameOver)
+		{
+			points += Time.deltaTime;
+			//textPoints.text = ((int)(points*10.0f)) + "";
+			
+			if(ball.transform.position.y <= ball.GetComponent<Ball>().roomY.x+1)
+			{
+				gameOver = true;
+				ball.GetComponent<Ball>().spriteRenderer.color = ball.GetComponent<Ball>().colorHalf;
+				//titleGameOver.SetActive(true);
+			}
+		}
+    }
+}
